85199e062398f03bd31ae17832a9ae1e85e14cc7
Sorin <mihaiirimies@gmail.com>
2016-09-11 16:07:49 +0200

    removed log extensions added location ext
    as anko provides löog extensions there is no need
    added lcoation extension function

---
+++ kotlin-ext/src/main/java/com/greenspand/kotlin_ext/LocationExt.kt
@@ -0,0 +1,22 @@
+package com.greenspand.kotlin_ext
+
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import java.util.*
+
+/**
+ * Created by mihai on 9/11/2016.
+ */
+/**
+ * Gets a complete address from anywhere where there is a provided context
+ */
+fun Context.getStreetAddress(lat: Double, lng: Double): List<Address>? {
+    try {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address>? = geocoder.getFromLocation(lat, lng, 1)
+        return addresses
+    } catch (e: Exception) {
+        return null
+    }
+}
--- kotlin-ext/src/main/java/com/greenspand/kotlin_ext/StorageExt.kt
+++ kotlin-ext/src/main/java/com/greenspand/kotlin_ext/StorageExt.kt
@@ -33,3 +33,5 @@ fun SharedPreferences.Editor.setInt(pair: Pair<String, Int>) = putInt(pair.first
 fun SharedPreferences.Editor.setBoolean(pair: Pair<String, Boolean>) = putBoolean(pair.first, pair.second)
 fun SharedPreferences.Editor.setFloat(pair: Pair<String, Float>) = putFloat(pair.first, pair.second)
 fun SharedPreferences.Editor.setLong(pair: Pair<String, Long>) = putLong(pair.first, pair.second)
+
+
